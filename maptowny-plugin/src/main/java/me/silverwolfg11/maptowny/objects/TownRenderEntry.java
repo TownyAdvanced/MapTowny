5d88c4947ff8ee4192892235de3deccc0daf8ade
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-09-08 01:11:26 +0200

    Use proper optional method for town homeblock points (#71)

--- maptowny-plugin/src/main/java/me/silverwolfg11/maptowny/objects/TownRenderEntry.java
+++ maptowny-plugin/src/main/java/me/silverwolfg11/maptowny/objects/TownRenderEntry.java
@@ -120,7 +120,7 @@ public class TownRenderEntry {
 
     @NotNull
     public Optional<Point2D> getHomeBlockPoint() {
-        return Optional.of(homeBlockPoint);
+        return Optional.ofNullable(homeBlockPoint);
     }
 
     public boolean hasWorldBlocks() {
